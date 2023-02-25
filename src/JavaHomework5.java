68c4f735a7d66ca8356969643fd4e227feb9f225
maxim_stoma <maksim.stoma@mail.ru>
2023-02-26 02:41:45 +0300

    created a basic laptop collection

---
+++ src/JavaHomework5.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class JavaHomework5 {
+
+    public static HashSet<Laptops> LaptopCollection(){
+        Laptops laptop1 = new Laptops();
+        laptop1.brand = "brand1";
+        laptop1.os = "os1";
+        laptop1.ram = 16;
+        laptop1.hardDrive = 512;
+        laptop1.cpuFrequency = 3.11F;
+        laptop1.screenDiagonal = 13.3F;
+
+        Laptops laptop2 = new Laptops();
+        laptop2.brand = "brand2";
+        laptop2.os = "os2";
+        laptop2.ram = 8;
+        laptop2.hardDrive = 1024;
+        laptop2.cpuFrequency = 2.56F;
+        laptop2.screenDiagonal = 15.1F;
+
+        Laptops laptop3 = new Laptops();
+        laptop3.brand = "brand3";
+        laptop3.os = "os3";
+        laptop3.ram = 16;
+        laptop3.hardDrive = 1024;
+        laptop3.cpuFrequency = 3.23F;
+        laptop3.screenDiagonal = 16.2F;
+
+        return new HashSet<Laptops>(Arrays.asList(laptop1, laptop2, laptop3));
+    }
+    public static void main(String[] args) {
+        var laptopColl = LaptopCollection();
+        System.out.println(laptopColl);
+    }
+}
